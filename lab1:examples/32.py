7e2c5dab714ec824756a0332c280688ef13f3b86
Dulat <dulatsultanbekov05@gmail.com>
2024-01-21 18:15:12 +0600

    lab1

---
+++ lab1:examples/32.py
@@ -0,0 +1,33 @@
+"""
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+
+"""
+x = 5
+print(type(x))
+
+"""
+In Python, the data type is set when you assign a value to a variable:
+x = "Hello World" => data type is string
+x = 20 => data type is int
+x = 20.5 => data type is float
+x = 1j => data type is complex
+x = ["apple", "banana", "cherry"] => data type is list
+x = ("apple", "banana", "cherry") => data type is tuple
+x = range(6) => data type is range
+x = {"name" : "John", "age" : 36} => data type is dict
+x = {"apple", "banana", "cherry"} => data type is set
+x = frozenset({"apple", "banana", "cherry"}) => data type is frozenset
+x = True => data type is bool
+x = b"Hello" => data type is bytes
+x = bytearray(5) => data type is bytearray
+x = memoryview(bytes(5)) => data type is memoryview
+x = None => data type is Nonetype
+
+"""
