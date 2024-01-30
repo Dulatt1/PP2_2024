ed9f5f0849b7649d0e55d8ef258ff326719b001d
Dulat <dulatsultanbekov05@gmail.com>
2024-01-30 15:50:37 +0600

    lab3

---
+++ lab3/functions1/5.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+
+def print_permutations():
+    s = input("String : ")
+    perms = permutations(s)
+    
+    print("All permutations : ")
+    for perm in perms:
+        print(''.join(perm))
+
+print_permutations()
